ee62a5f22811f471dc24804ef953d0e30b7dacbf
nulldayexploits <nulldayexploits1117@gmail.com>
2022-02-18 08:51:09 +0800

    okkk

--- admin/index.php
+++ admin/index.php
@@ -11,8 +11,9 @@
 
   <!-- Services -->
   <div class="w3-container" id="tentang" style="margin-top:45px;font-weight: bold;font-size: 20px;">
-    <center><BR><br><br>
+    <center><BR>
     	<h1>SELAMAT DATANG</h1> 
+      <img src="assets/dashboard.jpg">
       </center>
   </div>
   
